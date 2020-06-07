4a8c68e047fae179dc30d49adcf80a83414f460a
Ilya Strukov <iley@iley.ru>
2020-06-07 18:59:11 +0200

    Only allow authorized users

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"image/color"
+	"strings"
 
 	tg "github.com/go-telegram-bot-api/telegram-bot-api"
 	log "github.com/sirupsen/logrus"
@@ -21,6 +22,7 @@ func main() {
 	rstPin := flag.String("reset", "GPIO0", "name of the reset GPIO pin")
 	debug := flag.Bool("debug", false, "enable debug logging")
 	token := flag.String("token", "", "Telegram API token")
+	usersStr := flag.String("users", "", "Telegram usernames of the allowed users (comma-separated)")
 	flag.Parse()
 
 	if *debug {
@@ -29,6 +31,17 @@ func main() {
 
 	if *token == "" {
 		log.Fatalf("Please specify Telegram API token via --token")
+	}
+
+	users := parseList(*usersStr)
+	if len(users) == 0 {
+		log.Fatalf("Please specify a list of allowed telegram users via --users")
+	}
+	log.Debugf("allowed users: %v", users)
+
+	userMap := map[string]bool{}
+	for _, user := range users {
+		userMap[user] = true
 	}
 
 	log.Debugf("initializing host...")
@@ -57,21 +70,47 @@ func main() {
 	updChan, err := bot.GetUpdatesChan(upd)
 
 	for u := range updChan {
+		username := u.Message.From.UserName
+
+		if !userMap[username] {
+			log.Warnf("unauthorized user: %s", username)
+			msg := tg.NewMessage(u.Message.Chat.ID, "I don't know you. Go away!")
+			bot.Send(msg)
+			continue
+		}
+
 		if u.Message == nil { // ignore any non-Message Updates
 			continue
 		}
 
-		log.Debugf("[%s] %s", u.Message.From.UserName, u.Message.Text)
-		scr.Clear()
+		if u.Message.Text != "" {
+			log.Debugf("Text message from %s: %s", username, u.Message.Text)
+			scr.Clear()
 
-		disp := screen.NewDisplayer(scr)
-		tinyfont.WriteLine(disp, &tinyfont.Org01, 0, 5, []byte(u.Message.Text), white)
-		err = disp.Display()
-		if err != nil {
-			log.Fatalf("display error: %v", err)
+			disp := screen.NewDisplayer(scr)
+			tinyfont.WriteLine(disp, &tinyfont.Org01, 0, 5, []byte(u.Message.Text), white)
+			err = disp.Display()
+			if err != nil {
+				log.Fatalf("display error: %v", err)
+			}
+
+			msg := tg.NewMessage(u.Message.Chat.ID, "Message delivered")
+			bot.Send(msg)
 		}
 
-		msg := tg.NewMessage(u.Message.Chat.ID, "message delivered")
-		bot.Send(msg)
+		if u.Message.Photo != nil {
+			log.Debugf("Photo message from %s", u.Message.From.UserName)
+		}
 	}
+}
+
+func parseList(str string) []string {
+	res := []string{}
+	for _, tok := range strings.Split(str, ",") {
+		tok := strings.TrimSpace(tok)
+		if tok != "" {
+			res = append(res, tok)
+		}
+	}
+	return res
 }
